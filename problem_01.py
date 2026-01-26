1dbb5ab2dfd318da93bd43d0331aa54208803875
sahana haragapure <sahanaharagapure178@gmail.com>
2026-01-26 22:15:59 +0530

    Add files via upload

---
+++ problem_01.py
@@ -0,0 +1,14 @@
+class programmers:
+    def __init__(self,name,place,language):
+        self.name=name
+        self.place=place
+        self.language=language
+    
+sahana=programmers("sahana","belguam","java")
+rahul=programmers("rahul","bgm","java script")
+bharath=programmers("bharath","bijapur","python")
+
+
+print(sahana.name,sahana.place,sahana.language)
+print(rahul.name,rahul.place,rahul.language)
+print(bharath.name,bharath.place,bharath.language)
